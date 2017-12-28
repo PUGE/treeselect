7eaeffd1a212052699e968016e540e14e5e2d601
PUGE <100284685@qq.com>
2017-12-28 14:25:18 +0800

    完善功能

--- Tree.vue
+++ Tree.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="tree-select-box">
     <div class="tree-select-item" v-for="(item, ind) in data" :key="ind">
-      <label class="tree-select-item-bar">
-        <input name="treeSelect" type="checkbox" :value="item.label" v-model="checkedList[0]" @change="checkBoxClick($event.target, item)"/>
-        {{item.label}}
-      </label>
-      <div class="children-box" v-if="item.children">
-        <label class="children-item" v-for="(children, index) in item.children" :key="index">
-          <input name="treeSelect" type="checkbox" value="" v-model="checkedList[1]" @change="checkBoxClick($event, item)" />
+      <div class="tree-select-item-bar label">
+        <input name="treeSelect" type="checkbox" :value="item.label" v-model="checkedList[0]" @change.stop="checkBoxClick($event.target, item)"/>
+        <p @click.stop="clickBar(ind)">{{item.label}}</p>
+        <!-- 判断是否有子项目，如果有显示角标 -->
+        <div v-if="item.children" class="childrenNumber">({{item.children.length}})</div>
+      </div>
+      <div class="children-box" v-if="item.children" :style="getHeight(ind, item.children.length)">
+        <div class="children-item label" v-for="(children, index) in item.children" :key="index">
+          <input name="treeSelect" type="checkbox" :value="children.value || children.label" v-model="checkedList[1]" @change.stop="checkBoxClick($event.target, children, item)" />
           {{children.label}}
-        </label>
+        </div>
       </div>
     </div>
   </div>
@@ -28,18 +30,30 @@
         checkedList: [
           [],
           []
-        ]
+        ],
+        show: {}
       }
     },
     methods: {
-      checkBoxClick (event, target) {
+      checkBoxClick (event, target, parent) {
+        console.log(event)
         const sendData = {
           checked: event.checked,
           target,
-          checkedList: this.checkedList
+          checkedList: this.checkedList,
+          parent
         }
         console.log(sendData)
         this.$emit('onCheck', sendData)
+      },
+      clickBar (ind) {
+        if (this.show[ind]) this.show[ind] = this.$set(this.show, ind, false)
+        else this.show[ind] = this.$set(this.show, ind, true)
+      },
+      getHeight (ind, length) {
+        if (this.show[ind]) {
+          return { height: length * 30 + 'px' }
+        }
       }
     }
   }
@@ -47,29 +61,50 @@
 
 <style scoped>
   .tree-select-box {
-    width: 100%;
-    height: 100%;
-    background-color: white;
+    width: calc(100% - 20px);
+    height: calc(100% - 10px);
+    overflow-x: hidden;
+    overflow-y: auto;
+    background: #EEE;
+    padding: 10px 5px;
   }
   .children-box {
-    margin-left: 20px;
+    transition: height 0.2s;
+    height: 0;
+    overflow: hidden;
   }
-  label {
-    height: 25px;
-    line-height: 25px;
-    display: block;
+  .children-box .label {
+    padding-left: 20px;
+  }
+  .show {
+    height: auto;
+  }
+  .label {
+    height: 30px;
+    line-height: 30px;
     font-size: 0.8rem;
     display: flex;
+    cursor: pointer;
+    color: #008cba;
   }
-  label input {
-    height: 15px;
-    width: 15px;
-    margin: 5px;
+  .label:hover {
+    background-color: #008cba;
+    color: white;
+  }
+  .label input {
+    height: 16px;
+    width: 16px;
+    margin: 7px;
+  }
+  .label p {
+    width: calc(100% - 65px);
+    font-size: 0.9rem;
   }
   .tree-select-item-bar {
     border-bottom: 1px solid #ccc;
-    padding: 0 5px;
-    background-color: gainsboro;
-    color: #333;
+    margin: 0 10px;
+    padding: 2px 0;
+    color: #008cba;
+    font-weight: bold;
   }
 </style>
